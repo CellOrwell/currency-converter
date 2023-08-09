f5f976235fb7d7b775f009b085b82f18d89e0af9
CellOrwell <joshuamilroy@hotmail.co.uk>
2023-08-09 19:59:43 +0100

    Add app.js code - Testing Express.js and adding needed variables

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const axios = require('axios');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+const countries_url = "https://countryapi.io/api/name/austria";
 
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.redirect('/convert');
+})
 
 app.get('/convert', (req, res) => {
     const amount = req.query.amount;
     const from = req.query.from;
     const to = req.query.to;
+
+    const options = JSON.parse(process.env.CURRENCY_OPTIONS);
+
+    console.log(options);
+    res.send("Sent!");
 });
 
 
